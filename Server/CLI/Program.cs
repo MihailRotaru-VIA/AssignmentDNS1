6d02743081ad0cf36a9c2cd83e0512d9576fce35
mihai <355401@via.dkk>
2025-09-18 23:19:31 +0200

    Missing file :P

---
+++ Server/CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using CLI.UI;
+using InMemoryRepository;
+using RepositoryContracts;
+
+Console.WriteLine("Starting the CLI, please wait...\n");
+IUserRepository userRepository = new UserInMemoryRepository();
+ICommentRepository commentRepository = new CommentInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+
+ClientApp clientApp = new ClientApp(userRepository, commentRepository, postRepository);
+await clientApp.StartAsync();
